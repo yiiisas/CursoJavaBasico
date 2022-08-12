83aa0f557ad132285bfe344a66af1bd12a53fc2c
yiiisas <fedelx95@gmail.com>
2022-08-12 11:00:16 -0300

    agtdfgh

---
+++ 2-1/src/com/yisas/Datos.java
@@ -0,0 +1,28 @@
+package com.yisas;
+
+public class Datos {
+    public static void main(String[] args){
+        byte variable1 = 5;
+        System.out.println(variable1);
+        short variable2 = 10;
+        System.out.println(variable2);
+        int variable3 = 30;
+        System.out.println(variable3);
+        long variable4 = 100;
+        System.out.println(variable4);
+        float variable5 = 5.5f;
+        System.out.println(variable5);
+        double variable6 = 10.5d;
+        System.out.println(variable6);
+        boolean variable7 = false;
+        System.out.println(variable7);
+        boolean variable8 = true;
+        System.out.println(variable8);
+        char variable9 = 'a';
+        System.out.println(variable9);
+        String variable10 = "Lorem ipsum dolor sit amet.....";
+        System.out.println(variable10);
+    }
+}
+
+
---
+++ 3-1/Funciones/src/com/yisas/Funciones.java
@@ -0,0 +1,12 @@
+package com.yisas;
+
+public class Funciones {
+    public static void main(String[] args) {
+       double precioTotal = obtenerPrecioConIVA(120.56, 1.21);
+       System.out.println(precioTotal);
+    }
+
+    private static double obtenerPrecioConIVA(double precio, double iva) {
+        return precio * iva;
+    }
+}
